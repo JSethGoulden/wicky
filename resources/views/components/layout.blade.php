f4a038da98b11c94841e8182add873a68e9c5755
JSethGoulden <30304177+JSethGoulden@users.noreply.github.com>
2024-03-27 20:27:28 -0500

    cleanup

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,14 +9,6 @@
 
     <!-- Tailwind -->
     <script src="https://cdn.tailwindcss.com"></script>
-
-    <style>
-        @media (min-width: 768px) {
-            .grid-cols-1>* {
-                margin-right: 0;
-            }
-        }
-    </style>
 </head>
 
 <body class="antialiased bg-slate-200 text-slate-700 mx-2">

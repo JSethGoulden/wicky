e74174ea1d554b69906a6bc3881d5043081970f6
JSethGoulden <30304177+JSethGoulden@users.noreply.github.com>
2024-04-04 18:33:22 -0500

    cache reddit results

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
@@ -9,10 +10,11 @@ Route::get('/todays-travelling-merchant-shop', function () {
 });
 
 Route::get('/nemi-forest', function () {
-    $response = Http::withoutVerifying()->get('https://www.reddit.com/r/nemiforest/new.json');
-
-    return [
-        'thumbnail' => $response->json()['data']['children'][0]['data']['thumbnail'],
-        'img' => $response->json()['data']['children'][0]['data']['url_overridden_by_dest'],
-    ];
+    return Cache::remember('nemi-forest', now()->addHour(), function() {
+        $response = Http::withoutVerifying()->get('https://www.reddit.com/r/nemiforest/new.json');
+        return [
+            'thumbnail' => $response->json()['data']['children'][0]['data']['thumbnail'],
+            'img' => $response->json()['data']['children'][0]['data']['url_overridden_by_dest'],
+        ];
+    });
 });

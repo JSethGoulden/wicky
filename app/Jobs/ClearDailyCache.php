b26896e710835db4f34c1d00d96e1d46e6a14554
JSethGoulden <30304177+JSethGoulden@users.noreply.github.com>
2024-04-06 07:46:48 -0500

    daily cache purge

---
+++ app/Jobs/ClearDailyCache.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Cache;
+
+class ClearDailyCache implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        //todo consider switching cache to memcache or something
+        // to use tags instead of this
+        Cache::forget('nemi-forest');
+    }
+}
--- routes/console.php
+++ routes/console.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Jobs\ClearDailyCache;
 use App\Jobs\ProcessDailyActivities;
 use App\Jobs\ProcessMonthlyActivities;
 use App\Jobs\ProcessWeeklyActivities;
 use Illuminate\Support\Facades\Schedule;
+
+Schedule::job(new ClearDailyCache)
+    ->daily()
+    ->timezone('UTC')
+    ->at('00:00');
 
 Schedule::job(new ProcessDailyActivities)
     ->daily()
